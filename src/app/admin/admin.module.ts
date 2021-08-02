d57126f041a5edaee6435af02486ebb5a6ee9551
trandung <trandung164.ptit@gmail.com>
2021-08-02 17:18:21 +0700

    generate dashboard component

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdminRoutingModule } from './admin-routing.module';
+import { DashboardComponent } from './pages/dashboard.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    DashboardComponent
+  ],
   imports: [
     CommonModule,
     AdminRoutingModule
